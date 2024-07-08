f87044eba539dbe8aa99bf14c25b0353db4c57a3
Ivan Knezevic <knezevicivan500@gmail.com>
2024-07-08 13:09:30 +0200

    Create controller for register user.

---
+++ controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+  require_once "repository/UserRepository.php";
+  require_once "models/User.php";
+
+  class UserController {
+    private $userRepository;
+
+    public function __construct(IUserRepository $userRepository)
+    {
+      $this->userRepository = $userRepository;
+    }
+
+    public function registerUser($firstName, $lastName, $email, $password, $role, $profileImage, $phoneNumber) {
+      $newUser = new User();
+      $newUser->firstName = $firstName;
+      $newUser->lastName = $lastName;
+      $newUser->email = $email;
+      $newUser->password = password_hash($password, PASSWORD_BCRYPT);
+      $newUser->role = $role;
+      $newUser->profileImage = $profileImage;
+      $newUser->phoneNumber = $phoneNumber;
+
+      $this->userRepository->registerUser($newUser);
+
+      return "User added successfully!";
+    }
+  }
+
+?>
