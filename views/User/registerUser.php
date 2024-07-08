e311f31e5539a3b1c27fe6dc24bfe669d4ccde87
Ivan Knezevic <knezevicivan500@gmail.com>
2024-07-08 13:30:23 +0200

    Created form for register user

---
+++ views/User/registerUser.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register User</title>
+</head>
+<body>
+
+  <h1>Register User</h1>
+  <form action="" method="POST">
+    <label for="firstName">First Name:</label>
+    <input type="text" name="firstName" id="firstName" required><br><br>
+
+    <label for="lastName">Last Name:</label>
+    <input type="text" name="lastName" id="lastName" required><br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" required><br><br>
+
+    <label for="role">Role:</label>
+    <input type="text" name="role" id="role" required><br><br>
+
+    <label for="prfileImage">Profile Image:</label>
+    <input type="text" name="prfileImage" id="prfileImage" required><br><br>
+
+    <label for="phoneNumber">Phone Number:</label>
+    <input type="int" name="phoneNumber" id="phoneNumber" required><br><br>
+  </form>
+
+</body>
+</html>
