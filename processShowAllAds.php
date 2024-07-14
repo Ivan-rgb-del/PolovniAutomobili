eb017397a60860ba8c1efa40fd774498ea9d52bb
Ivan Knezevic <knezevicivan500@gmail.com>
2024-07-14 20:49:04 +0200

    Makes to see images of ads

--- processShowAllAds.php
+++ processShowAllAds.php
@@ -1,12 +1,14 @@
 <?php
 
   require_once "controllers/AdvertisementController.php";
+  require_once "controllers/AdImageController.php";
   require_once "repository/AdImageRepository.php";
   require_once "repository/AdvertisementRepository.php";
 
   $adImageRepo = new AdImageRepository();
   $adRepo = new AdvertisementRepository();
   $adController = new AdvertisementController($adRepo, $adImageRepo);
+  $adImageController = new AdImageController($adImageRepo);
 
   $ads = $adController->getAllAdvertisement();
 
@@ -25,6 +27,13 @@
 
   <?php foreach($ads as $ad): ?>
     <h3> <?= $ad['title'] ?> </h3>
+
+    <?php if ($adImageController->getImageOfAd($ad['id'])): ?>
+      <img src="assets/pictures/AdPictures/<?= $adImageController->getImageOfAd($ad['id']) ?>" alt="Ad picture" style="width: 250px;">
+      <?php else: ?>
+        <img src="assets/pictures/AdPictures/notFound.png" alt="Not found" style="width: 250px;">
+    <?php endif; ?>
+
     <p>Price: <?= number_format($ad['price']) ?>€ </p>
     <p>Year: <?= $ad['first_registration'] ?> </p>
     <p>Fuel type: <?= $ad['fuel_type'] ?> </p>
