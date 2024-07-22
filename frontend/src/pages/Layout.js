f3044b9e266a6000b364a09f40d83800127c2994
Ivan Knezevic <knezevicivan500@gmail.com>
2024-07-22 15:28:14 +0200

    Change Layout for navigation

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -3,7 +3,7 @@ import { Outlet, Link } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 
 const Layout = () => {
-  const { logged, handleLogout } = useContext(UserContext);
+  const { logged, userRole, handleLogout } = useContext(UserContext);
 
   return (
     <div>
@@ -13,9 +13,19 @@ const Layout = () => {
             <Link to="/login-user">Login</Link>
             <Link to="/register-user">Register</Link>
           </div>
-        ) : (
-          <button onClick={handleLogout}>Logout</button>
-        )}
+        ) : userRole === 'seller' ? (
+          <div>
+            <button onClick={handleLogout}>Logout</button>
+            <Link to="/add-new-ad">Add New Ad</Link>
+            <Link to="/my-ads">My Ads</Link>
+          </div>
+        ) : userRole === 'user' ? (
+          <div>
+            <button onClick={handleLogout}>Logout</button>
+            <Link to="/look-for-new-ads">Look for New Ads</Link>
+            <Link to="/saved-ads">Saved Ads</Link>
+          </div>
+        ) : null}
       </nav>
       <Outlet />
     </div>
